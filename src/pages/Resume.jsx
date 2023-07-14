086423919e061481677508d4804296559519a328
anishap123 <anishapoudel570@gmail.com>
2023-07-14 13:23:14 +0545

    resume page design

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
      <div className="basis-[58%] border h-[85vh] mt-10 ml- -2 mr-8 bg-white rounded-r-md  overflow-auto">
       <div className="About flex flex-col ">
         <div className="text-[25px] py-4 ml-4 font-bold text-[#4c7753] cursor-pointer ">
-          Resume
+          Resume page
         </div>
         <hr className=" flex my-2  h-full w-full bg-gray-800 " />
        
